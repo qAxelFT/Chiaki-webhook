ca83be8fd2f4febe6d5d92d4b8b83c82b67dbef3
clara <claracarrasquelfiorini@gmail.com>
2021-05-02 00:03:57 -0500

    Update event.py

---
+++ src/event.py
@@ -0,0 +1,47 @@
+import datetime
+import json
+import os
+
+
+# Enum for the days of the week
+weekday = (
+    "Lunes",
+    "Martes",
+    "Miercoles",
+    "Jueves",
+    "Viernes",
+    "Sabado",
+    "Domingo"
+)
+
+
+# The time at which Chiaki acts each day of the week
+# It contains 7 items for each of the 7 days of the week
+# Each item contains 2 items: (hour, minute)
+timetable = (
+    (7, 00),
+    (7, 00),
+    (7, 00),
+    (7, 00),
+    (7, 00),
+    (12, 00),
+    (12, 00)
+)
+
+
+def Today():
+    return weekday[datetime.datetime.now().weekday() % len(weekday)]
+
+
+def Tomorrow():
+    return (datetime.datetime.now().weekday() + 1) % len(weekday)
+
+
+def Image():
+    with open(f"{os.getcwd()}/assets/images.json", "r") as file:
+        return json.load(file)[Today()]
+
+
+def Footer():
+    with open(f"{os.getcwd()}/assets/images.json", "r") as file:
+        return json.load(file)["Footer"]
