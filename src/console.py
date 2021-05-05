ebeadce5d128dcaa3e5cd2b8d34b3255553085ea
clara <claracarrasquelfiorini@gmail.com>
2021-05-05 10:16:49 -0500

    Added functions for new flags system and improved logging

--- src/console.py
+++ src/console.py
@@ -1,4 +1,9 @@
+from src.parse import args as arguments
+from threading import Thread
+import pathlib as pl
+import code
 import time
+import sys
 
 Off = '\033[0m'           # Text Reset
 Black = '\033[1;90m'      # Black
@@ -11,12 +16,38 @@ Cyan = '\033[1;96m'       # Cyan
 White = '\033[1;97m'      # White
 
 
-def out(*args):
-    print(str().join(args))
+def Interact(globals_var):
+    if arguments.interactive or arguments.persistent:
+        t = Thread(target=code.InteractiveConsole(locals=globals_var).interact)
+        t.start()
+        return t
+    return Thread()
 
 
-def log(filename, *args):
-    with open(filename, "a") as logfile:
-        logfile.write(f"{Blue}{time.asctime()}{Off}")
-        [logfile.write(f"\t{statement}\n") for statement in args]
-        logfile.write('\n')
+def Join(thread: Thread):
+    if arguments.persistent:
+        if thread.is_alive():
+            thread.join(timeout=30)
+
+
+def Out(*args):
+    print(str().join(args) if not arguments.silent else '')
+
+
+def Log(filename, *args):
+    try:
+        with open(filename, "a") as logfile:
+            logfile.write(f"{Blue}{time.asctime()}{Off}")
+            [logfile.write(f"\t{statement}\n") for statement in args]
+            logfile.write('\n')
+
+    except FileNotFoundError:
+        pl.Path(''.join([f"/{i}" for i in filename.split('/')[0:-1] if i != ''])).mkdir(parents=True, exist_ok=True)
+        Log(filename, args)
+
+
+def Verbose(*args):
+    Log(f"{sys.path[0]}/assets/logs/runtime.log")
+    if arguments.verbose or arguments.very_verbose:
+        Out(*args)
+        Log(f"{sys.path[0]}/assets/logs/verbose.log")
