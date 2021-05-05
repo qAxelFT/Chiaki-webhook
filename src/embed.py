4c748fff5b2255a09f2608bac15013a91ba456cf
clara <claracarrasquelfiorini@gmail.com>
2021-05-05 10:17:17 -0500

    Added verbose output

--- src/embed.py
+++ src/embed.py
@@ -1,5 +1,7 @@
-from src.event import Today, Image, Footer
+from src.event import Today, Image, Footer, weekday_en
 from src.randomPhrases import phrase
+import datetime
+import src.console as console
 
 
 def Embed():
@@ -13,5 +15,11 @@ def Embed():
     message = dict()
     message["content"] = f"<@&832774619344666634> {phrase()}"
     message["embeds"] = [{"title": title, "color": color, "image": {"url": Image()}, "footer": footer}]
+
+    # Verbose output
+    prefix = f"{console.Green}[+]{console.Off}"
+    console.Verbose(f"{prefix} Today is {weekday_en[datetime.datetime.now().weekday()]}")
+    console.Verbose(f"{console.Purple}*{console.Off} Embed Title: {title}")
+    console.Verbose(f"{console.Purple}*{console.Off} Embed Image Link: {Image()}")
 
     return message
